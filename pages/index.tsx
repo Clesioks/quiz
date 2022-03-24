353fd17fff64ad7f9f19668ea2de0140317cf958
Clesioks <kornalewski@outlook.com>
2022-03-24 13:11:01 -0300

    componentes

---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+import Questao from "../components/Questao";
+import QuestaoModel from "../model/questao";
+import RespostaModel from "../model/resposta";
+
+export default function Home() {
+  const questaoTeste = new QuestaoModel(1, "What's color of ocean?", [
+    RespostaModel.errada("Green"),
+    RespostaModel.errada("Red"),
+    RespostaModel.errada("black"),
+    RespostaModel.certa("Blue"),
+  ]);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Questao valor={questaoTeste} />
+    </div>
+  );
+}
