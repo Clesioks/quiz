6f014c152060b627f99dd4d735905cd8ef2e621b
Clesioks <kornalewski@outlook.com>
2022-03-28 18:53:04 -0300

    layout respostas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import Questao from "../components/Questao";
 import QuestaoModel from "../model/questao";
 import RespostaModel from "../model/resposta";
 
+const questaoMock = new QuestaoModel(1, "What's color of ocean?", [
+  RespostaModel.errada("Green"),
+  RespostaModel.errada("Red"),
+  RespostaModel.errada("black"),
+  RespostaModel.certa("Grêmio"),
+]);
+
 export default function Home() {
-  const questaoTeste = new QuestaoModel(1, "What's color of ocean?", [
-    RespostaModel.errada("Green"),
-    RespostaModel.errada("Red"),
-    RespostaModel.errada("black"),
-    RespostaModel.certa("Grêmio"),
-  ]);
+  const [questao, setQuestao] = useState(questaoMock);
+
+  function respostaFornecida(indice: number) {
+    console.log(indice);
+    setQuestao(questao.responderCom(indice));
+  }
 
   return (
     <div
@@ -19,7 +27,7 @@ export default function Home() {
         height: "100vh",
       }}
     >
-      <Questao valor={questaoTeste} />
+      <Questao valor={questao} respostaFornecida={respostaFornecida} />
     </div>
   );
 }
