fb49e94969ec3987a63f76987aa1b6d79d5a3ece
Clesioks <kornalewski@outlook.com>
2022-04-20 14:27:55 -0300

    button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,20 @@
 import { useEffect, useState } from "react";
 import Questionario from "../components/Questionario";
 import QuestaoModel from "../model/questao";
-import RespostaModel from "../model/resposta";
-
-const questaoMock = new QuestaoModel(1, "What's color of ocean?", [
-  RespostaModel.errada("Green"),
-  RespostaModel.errada("Red"),
-  RespostaModel.errada("black"),
-  RespostaModel.certa("Grêmio"),
-]);
+import { useRouter } from "next/router";
 
 const BASE_URL = "http://localhost:3000/api";
 
 export default function Home() {
+  const router = useRouter();
+
   const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([]);
-  const [questao, setQuestao] = useState<QuestaoModel>(questaoMock);
+  const [questao, setQuestao] = useState<QuestaoModel>();
+  const [respostasCertas, setRespostasCertas] = useState<number>(0);
 
   async function carregarIdsDasQuestoes() {
     const resp = await fetch(`${BASE_URL}/questionario`);
     const idsDasQuestoes = await resp.json();
-    console.log(idsDasQuestoes);
     setIdsDasQuestoes(idsDasQuestoes);
   }
 
@@ -38,16 +33,44 @@ export default function Home() {
     idsDasQuestoes.length > 0 && carregarQuestoes(idsDasQuestoes[0]);
   }, [idsDasQuestoes]);
 
-  function questaoRespondida(questao: QuestaoModel) {}
+  function questaoRespondida(questaoRespondida: QuestaoModel) {
+    setQuestao(questaoRespondida);
+    const acertou = questaoRespondida.acertou;
+    setRespostasCertas(respostasCertas + (acertou ? 1 : 0));
+  }
 
-  function irPraProximoPasso() {}
+  function idProximaPergunta() {
+    const proximoIndice = idsDasQuestoes.indexOf(questao.id) + 1;
+    return idsDasQuestoes[proximoIndice];
+  }
 
-  return (
+  function irPraProximoPasso() {
+    const proximoId = idProximaPergunta();
+    proximoId ? irPraProximaQuestao(proximoId) : finalizar();
+  }
+
+  function irPraProximaQuestao(proximoId: number) {
+    carregarQuestoes(proximoId);
+  }
+
+  function finalizar() {
+    router.push({
+      pathname: "/resultado",
+      query: {
+        total: idsDasQuestoes.length,
+        certas: respostasCertas,
+      },
+    });
+  }
+
+  return questao ? (
     <Questionario
       questao={questao}
-      ultima={true}
+      ultima={idProximaPergunta() === undefined}
       questaoRespondida={questaoRespondida}
       irPraProximoPasso={irPraProximoPasso}
     />
+  ) : (
+    false
   );
 }
--- pages/resultado.tsx
+++ pages/resultado.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { useRouter } from "next/router";
+import styles from "../styles/Resultado.module.css";
 
 const resultado = () => {
-  return <div>Resultado</div>;
+  const router = useRouter();
+
+  const total = +router.query.total;
+  const certas = +router.query.certas;
+  const percentual = Math.round((certas / total) * 100);
+
+  return (
+    <div className={styles.resultado}>
+      <h1>Resultado Final</h1>
+      <span>{total}</span>
+      <span>{certas}</span>
+      <span>{`${percentual}%`}</span>
+    </div>
+  );
 };
 
 export default resultado;
